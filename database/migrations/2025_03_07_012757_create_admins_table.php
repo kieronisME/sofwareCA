d577d4e1f43c29fc71b55065b47f66b9a4657cd2
kieron <N00233918@iadt.ie>
2025-03-07 13:32:47 +0000

    getinhg migrations ready

--- database/migrations/2025_03_07_012757_create_admins_table.php
+++ database/migrations/2025_03_07_012757_create_admins_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('user_name');
+            $table->string('email');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('phoneNum')->nullable();
+            $table->
             $table->timestamps();
+
+
+            $table->foreign('')
         });
     }
 
